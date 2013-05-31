202337cb375ff3c4fb47239adc089ea0d8d2844b
Chris Guilbeau <chris.guilbeau@gmail.com>
2013-05-31 15:31:48 +0000

    added deploy script

---
+++ meteoric.config.sh
@@ -0,0 +1,14 @@
+# IP or URL of the server you want to deploy to
+APP_HOST=107.20.251.237
+
+# Comment this if your host is not an EC2 instance
+EC2_PEM_FILE=~/.ssh/hox-money.pem
+
+# What's your project's Git repo?
+GIT_URL=git://github.com/chrisguilbeau/money.git
+
+# Does your project use meteorite, or plain meteor?
+METEORITE=false
+
+# What's your app name?
+APP_NAME=money
